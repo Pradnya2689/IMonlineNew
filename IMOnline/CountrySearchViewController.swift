0f90ef78fa8bd5cae78d8a3026b59c7cc481b24c
Administrator <administrator@Administrators-iMac-2.local>
2017-04-10 15:42:23 +0530

    10thApril

--- IMOnline/CountrySearchViewController.swift
+++ IMOnline/CountrySearchViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class CountrySearchViewController: UIViewController, UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate,UIGestureRecognizerDelegate {
     @IBOutlet weak var countrySearchTableView: UITableView!
-
+    
     @IBOutlet weak var searchTextField: UITextField!
     
     var countryNameAAray = ["AUSTRALIA","CROATIA","ENGLAND","FRANCE","ICELAND","JORDAN"]
@@ -18,12 +18,12 @@ class CountrySearchViewController: UIViewController, UITableViewDelegate,UITable
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
-//        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-//        tap.delegate = self
-//        view.addGestureRecognizer(tap)
+        //        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        //        tap.delegate = self
+        //        view.addGestureRecognizer(tap)
     }
     
     func handleTap(_ sender: UITapGestureRecognizer? = nil){
@@ -37,6 +37,7 @@ class CountrySearchViewController: UIViewController, UITableViewDelegate,UITable
     }
     
     
+    // MARK: - Tableview Functions
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
@@ -55,28 +56,26 @@ class CountrySearchViewController: UIViewController, UITableViewDelegate,UITable
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        print(indexPath)
-        
         self.dismiss(animated: true, completion: nil)
-       // self.navigationController?.dismiss(animated: true, completion: nil)
+        
     }
     
     
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }

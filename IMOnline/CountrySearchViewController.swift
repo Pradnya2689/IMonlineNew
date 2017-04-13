cddfa161b559a0514d1db299bca7d8495be64512
pradnya <pradnya@admin-iMac2.local>
2017-04-13 12:21:05 +0530

    save

--- IMOnline/CountrySearchViewController.swift
+++ IMOnline/CountrySearchViewController.swift
@@ -29,15 +29,15 @@ class CountrySearchViewController: UIViewController, UITableViewDelegate,UITable
 //            micButton.isEnabled = false
 //            //micBtn.setTitle("Start Recording", for: .normal)
 //        }else{
-            startRecording()
+          //  startRecording()
             //micBtn.setTitle("Stop Recording", for: .normal)
         //}
     }
     
-    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
-    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
-    private var recognitionTask: SFSpeechRecognitionTask?
-    private let audioEngine = AVAudioEngine()
+//    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
+//    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+//    private var recognitionTask: SFSpeechRecognitionTask?
+//    private let audioEngine = AVAudioEngine()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,36 +47,36 @@ class CountrySearchViewController: UIViewController, UITableViewDelegate,UITable
         
         micButton.isEnabled = false
         
-        speechRecognizer?.delegate = self
+      //  speechRecognizer?.delegate = self
         
-        SFSpeechRecognizer.requestAuthorization{ (authStatus) in
-            
-            var isButtonEnabled = false
-            
-            switch authStatus{
-                
-            case .authorized:
-                isButtonEnabled = true
-                
-            case .denied:
-                isButtonEnabled = false
-                print("User denied access to speech recognition")
-                
-            case .restricted:
-                isButtonEnabled = false
-                print("Speech recognition restricted on this device")
-                
-            case .notDetermined:
-                isButtonEnabled = false
-                print("Speech recognition not yet authorized")
-                
-            }
-            
-            OperationQueue.main.addOperation {
-                self.micButton.isEnabled = isButtonEnabled
-            }
-            
-        }
+//        SFSpeechRecognizer.requestAuthorization{ (authStatus) in
+//            
+//            var isButtonEnabled = false
+//            
+//            switch authStatus{
+//                
+//            case .authorized:
+//                isButtonEnabled = true
+//                
+//            case .denied:
+//                isButtonEnabled = false
+//                print("User denied access to speech recognition")
+//                
+//            case .restricted:
+//                isButtonEnabled = false
+//                print("Speech recognition restricted on this device")
+//                
+//            case .notDetermined:
+//                isButtonEnabled = false
+//                print("Speech recognition not yet authorized")
+//                
+//            }
+//            
+//            OperationQueue.main.addOperation {
+//                self.micButton.isEnabled = isButtonEnabled
+//            }
+//            
+//        }
         
     }
     
@@ -89,103 +89,103 @@ class CountrySearchViewController: UIViewController, UITableViewDelegate,UITable
     
    // MARK: - Microphone Function
     
-    func startRecording() {
-        
-        if recognitionTask != nil {
-            recognitionTask?.cancel()
-            recognitionTask = nil
-        }
-        
-        let audioSession = AVAudioSession.sharedInstance()
-        do {
-            try audioSession.setCategory(AVAudioSessionCategoryRecord)
-            try audioSession.setMode(AVAudioSessionModeMeasurement)
-            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
-        } catch {
-            print("audioSession properties weren't set because of an error.")
-        }
-        
-        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
-        
-        guard let inputNode = audioEngine.inputNode else {
-            fatalError("Audio engine has no input node")
-        }
-        
-        guard let recognitionRequest = recognitionRequest else {
-            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
-        }
-        
-        recognitionRequest.shouldReportPartialResults = true
-        
-        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
-            
-            var isFinal = false
-            
+//    func startRecording() {
+//        
+////        if recognitionTask != nil {
+////            recognitionTask?.cancel()
+////            recognitionTask = nil
+////        }
+//        
+//        let audioSession = AVAudioSession.sharedInstance()
+//        do {
+//            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+//            try audioSession.setMode(AVAudioSessionModeMeasurement)
+//            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+//        } catch {
+//            print("audioSession properties weren't set because of an error.")
+//        }
+//        
+//        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+//        
+//        guard let inputNode = audioEngine.inputNode else {
+//            fatalError("Audio engine has no input node")
+//        }
+//        
+//        guard let recognitionRequest = recognitionRequest else {
+//            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+//        }
+//        
+//        recognitionRequest.shouldReportPartialResults = true
+//        
+//        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+//            
+//            var isFinal = false
+//            
+////            if result != nil {
+////                
+////                self.searchTextField.text = result?.bestTranscription.formattedString
+////                isFinal = (result?.isFinal)!
+////                
+////                self.textFieldDidChangeSiri()
+////                
+////            }
+////            
+////            
+////            if error != nil || isFinal {
+////                self.audioEngine.stop()
+////                inputNode.removeTap(onBus: 0)
+////                
+////                self.recognitionRequest = nil
+////                self.recognitionTask = nil
+////                
+////                self.micButton.isEnabled = true
+////            }
+//            
+//            
 //            if result != nil {
 //                
 //                self.searchTextField.text = result?.bestTranscription.formattedString
-//                isFinal = (result?.isFinal)!
 //                
-//                self.textFieldDidChangeSiri()
-//                
-//            }
-//            
-//            
-//            if error != nil || isFinal {
+////                self.cancelRecording()
 //                self.audioEngine.stop()
 //                inputNode.removeTap(onBus: 0)
-//                
 //                self.recognitionRequest = nil
 //                self.recognitionTask = nil
-//                
 //                self.micButton.isEnabled = true
+//                
+//                self.textFieldDidChangeSiri()
+//                                
 //            }
-            
-            
-            if result != nil {
-                
-                self.searchTextField.text = result?.bestTranscription.formattedString
-                
-//                self.cancelRecording()
-                self.audioEngine.stop()
-                inputNode.removeTap(onBus: 0)
-                self.recognitionRequest = nil
-                self.recognitionTask = nil
-                self.micButton.isEnabled = true
-                
-                self.textFieldDidChangeSiri()
-                                
-            }
-
-            
-            
-        })
-        
-        let recordingFormat = inputNode.outputFormat(forBus: 0)
-        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
-            self.recognitionRequest?.append(buffer)
-        }
-        
-        audioEngine.prepare()
-        
-        do {
-            try audioEngine.start()
-        } catch {
-            print("audioEngine couldn't start because of an error.")
-        }
-        
-        searchTextField.text = "Say something, I'm listening!"
-        
-    }
+//
+//            
+//            
+//        })
+//        
+//        let recordingFormat = inputNode.outputFormat(forBus: 0)
+//        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+//            self.recognitionRequest?.append(buffer)
+//        }
+//        
+//        audioEngine.prepare()
+//        
+//        do {
+//            try audioEngine.start()
+//        } catch {
+//            print("audioEngine couldn't start because of an error.")
+//        }
+//        
+//        searchTextField.text = "Say something, I'm listening!"
+//        
+//    }
     
     
-    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
-        if available {
-            micButton.isEnabled = true
-        } else {
-            micButton.isEnabled = false
-        }
-    }
+//    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+//        if available {
+//            micButton.isEnabled = true
+//        } else {
+//            micButton.isEnabled = false
+//        }
+//    }
     
     
     
